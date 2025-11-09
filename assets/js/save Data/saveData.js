c5ff851922e2b44d5d678e667932f122a97d8d83
aziz_boujaada <boujaadaaziz2911@gmail.com>
2025-11-09 17:47:36 +0100

    feat: enhance link management and add education data saving functionality

--- assets/js/save Data/saveData.js
+++ assets/js/save Data/saveData.js
@@ -46,13 +46,22 @@ function saveLinks() {
   const addLinkBtn = document.getElementById("add_link");
   const displayLinks = document.getElementById("display_links");
   addLinkBtn.addEventListener("click", () => {
-    const socialLinks = document.getElementById("links").value;
-    UserCvData.personalInfo.links.push(socialLinks);
-    if (socialLinks == "") return;
+    const socialLinks = document.getElementById("links").value.trim();
+    if (
+      socialLinks === "" ||
+      UserCvData.personalInfo.links.includes(socialLinks)
+    ) {
+      const errorDiv = document.createElement("p");
+      errorDiv.innerHTML = `<p class = " text-red-500">this link (${socialLinks}) already exsit</p>`;
+      displayLinks.appendChild(errorDiv);
+      return;
+    } else {
+      UserCvData.personalInfo.links.push(socialLinks);
+    }
     displayLinks.innerHTML = UserCvData.personalInfo.links
       .map(
         (link, index) =>
-          `<p class= "bg-gray-200 shadow-lg rounded-full p-1 px-2 mb-1 flex justify-between">
+          `<p class= "bg-gray-200 shadow-lg rounded-full p-1 px-2 my-2 flex justify-between">
         <span>${link}</span>
          <span class ="clear_link cursor-pointer hover:text-red-600" data-index = "${index}">
          <i class="fa-solid fa-trash"></i>
@@ -72,7 +81,7 @@ function saveLinks() {
     displayLinks.innerHTML = UserCvData.personalInfo.links
       .map(
         (link, index) =>
-          `<p class= "bg-gray-500 rounded-full p-1 mb-1">
+          `<p class= "bg-gray-300 rounded-full p-1 mb-1">
         <span>${link}</span>
          <span class ="clear_link cursor-pointer hover:text-red-600" data-index = "${index}">
          <i class="fa-solid fa-trash"></i>
@@ -83,6 +92,7 @@ function saveLinks() {
   });
 }
 export function saveUserData() {
+  //save  personal information
   const fullname = document.getElementById("full_name").value;
   const Email = document.getElementById("email").value;
   const phone = document.getElementById("phone").value;
@@ -96,10 +106,35 @@ export function saveUserData() {
   UserCvData.personalInfo.summary = summary;
 
   console.log(UserCvData.personalInfo);
-  localStorage.setItem(
-    "personalInformation",
-    JSON.stringify(UserCvData.personalInfo)
-  );
+
+  // save eduaction information
+
+  const saveEducationBtn = document.getElementById("save_education");
+  saveEducationBtn.addEventListener("click", (e) => {
+    const schoolName = document.getElementById("school_name").value.trim();
+    const startDate = document.getElementById("start_date").value.trim();
+    const endDate = document.getElementById("end_date").value.trim();
+    const schoolLocation = document
+      .getElementById("school_location")
+      .value.trim();
+    const educationDescription = document
+      .getElementById("education_description")
+      .value.trim();
+    e.preventDefault();
+    UserCvData.educationInfo.push({
+      schoolName,
+      startDate,
+      endDate,
+      schoolLocation,
+      educationDescription,
+    });
+
+    console.log("eduaction data", UserCvData.educationInfo)
+
+  });
+
+  //save data in localstorgae
+  localStorage.setItem("personalInformation", JSON.stringify(UserCvData));
 }
 
 saveLinks();
